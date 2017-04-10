4102bf0763c3c4c67e18850039a5e42689d5ba82
seanbass <seandbass@gmail.com>
2017-04-10 06:37:38 -0700

    init

---
+++ myList.py
@@ -0,0 +1,25 @@
+my_list = [0, 1, 2, 3]
+my_list_len = len(my_list)
+for i in range(4):
+    print (my_list[i], end=' ')
+
+
+print ("\n First \n")
+
+second_list = [3, 2, 1, 0]
+second_list_len = len(second_list)
+for x in range(4):
+    print (second_list[x], end=' ')
+
+
+print ("\n Second \n")
+
+
+third_list = [8, 6, 4, 2]
+third_list_len = len(third_list)
+for a in range(4):
+    print (third_list[a], end=' ')
+
+
+print ("\n Third \n")
+
